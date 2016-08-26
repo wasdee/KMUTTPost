1534fd9ade83c1a3b2004c1d3d2eca17d1c28188
Charles Whitesman <nutchanon.ben@mail.kmutt.ac.th>
2016-08-27 03:17:00 +0700

    hekko

--- check.py
+++ check.py
@@ -8,61 +8,10 @@ from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
 
-class hello(unittest.TestCase):
-    result = None
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "http://hermes.kmutt.ac.th/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-
-    def test_3ini(self):
-        driver = self.driver
-        driver.get(self.base_url + "/post/")
-        driver.find_element_by_id("txtSearch").clear()
-        driver.find_element_by_id("txtSearch").send_keys(u"ณัฐชนน")
-        driver.find_element_by_css_selector("#lbSearch > b").click()
-        try:
-            self.assertTrue(self.is_element_present(By.XPATH, "//table[@id='dgLetter']/tbody/tr[2]"))
-        except AssertionError as e:
-            self.verificationErrors.append(str(e))
-
-
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
-
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
-def isThereMyMailAtKMUTTOffice(remote=None,ip=None,port=None):
+def isThereMyMailAtKMUTTOffice():
     result = None
     try:
-        driver = webdriver.Remote("firefox/wd/hub", webdriver.DesiredCapabilities.FIREFOX.copy())
+        driver = webdriver.Remote("http://firefox/wd/hub", webdriver.DesiredCapabilities.FIREFOX.copy())
     except Exception as e:
         raise e
     # driver = webdriver.Firefox()
@@ -84,9 +33,3 @@ def isThereMyMailAtKMUTTOffice(remote=None,ip=None,port=None):
     finally:
         driver.quit()
     return result
-
-if __name__ == "__main__":
-    # unittest.main()
-    # hello.run()
-    # print(isThereMyMailAtKMUTTOffice())
-    print("iloveu")
